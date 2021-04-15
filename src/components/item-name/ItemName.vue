2438937c6fe77ec1b5e96c33e2416b1b7a411c7e
Stefan Peters <Stefan.Peters@gbv.de>
2021-04-15 14:04:47 +0200

    Update ItemName component

--- src/components/item-name/ItemName.vue
+++ src/components/item-name/ItemName.vue
@@ -1,7 +1,18 @@
 <template>
-  <span>
-    <b>{{ notation }}</b>
+  <span
+    :class="{
+      'item-name': true,
+      clickable,
+    }">
+    <span
+      v-if="showNotation || label === ''"
+      class="notation">
+      {{ notation }}
+    </span>
     {{ label }}
+    <template v-if="notation === '' && label === ''">
+      {{ fallbackLabel }}
+    </template>
   </span>
 </template>
 
@@ -24,20 +35,49 @@ export default defineComponent({
       type: Boolean,
       default: true,
     },
+    fallbackLabel: {
+      type: String,
+      default: "???",
+    },
+    language: {
+      type: String,
+      default: "en",
+    },
+    clickable: {
+      type: Boolean,
+      default: false,
+    },
   },
   setup(props) {
+    const notation = computed(() =>
+      props.showNotation
+        ? jskos.notation(props.item)
+        : "",
+    )
+    const label = computed(() =>
+      props.showLabel
+        ? jskos.prefLabel(props.item, {
+          fallbackToUri: !notation.value,
+          language: props.language,
+        })
+        : "",
+    )
     return {
-      notation: computed(() =>
-        props.showNotation
-          ? jskos.notation(props.item)
-          : "",
-      ),
-      label: computed(() =>
-        props.showLabel
-          ? jskos.prefLabel(props.item, { fallbackToUri: !props.showNotation })
-          : "",
-      ),
+      notation,
+      label,
     }
   },
 })
 </script>
+
+<style scoped>
+.notation {
+  font-weight: bold;
+}
+.item-name.clickable {
+  cursor: pointer;
+}
+.item-name.clickable:hover {
+  text-decoration: underline;
+}
+</style>
