ae5b1be8bd5a3f304952f2dc2a17cdf190cc2938
Stefan Peters <Stefan.Peters@gbv.de>
2021-06-14 11:15:38 +0200

    Adjust vite.config.js to allow building App in addition to library

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,16 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 const path = require("path")
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
+/**
+ * Allow two different builds:
+ * - `npm run build` builds the library
+ * - `BUILD_MODE=app npm run build` builds the app (App.vue)
+ *
+ * Note that the destination folder `dist` will be emptied before each build.
+ */
+let build
+if (process.env.BUILD_MODE !== "app") {
+  build = {
     lib: {
       entry: path.resolve(__dirname, "src/index.js"),
       name: "JskosVue",
@@ -21,5 +27,11 @@ export default defineConfig({
         },
       },
     },
-  },
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  build,
 })
