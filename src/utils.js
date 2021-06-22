865cc904322308e75202537657282240c9896b11
Stefan Peters <Stefan.Peters@gbv.de>
2021-06-22 10:48:41 +0200

    Utils: Add addClickHandlers mixin

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+import { onMounted, onUnmounted } from "vue"
+
 /**
  * Converts a date string to a localized date string.
  * Incomplete dates (YYYY or YYYY-MM) will be returned non-localized.
@@ -19,4 +21,48 @@ export function dateToString(dateString) {
   } else {
     return "?"
   }
+}
+
+/**
+ * Click handler mixin
+ *
+ * Handles clicks in the application, e.g. to hide popovers when clicked outside.
+ *
+ * Only works with the composition API. Takes a function as its first argument which returns an array.
+ * Each item in the array is an object with the following properties:
+ * - `elements`: An array of elements that the click should be checked against.
+ * - `handler`: A function that is called when a click outside the elements is registered.
+ *
+ * The second argument (optional, default: `false`) forces the handler to be called even if the click is inside the provided elements.
+ *
+ * Usually for popovers, you should add both the popover element (or the inside div rather) and the button that opens the popover to `elements`.
+ *
+ * Example:
+ * popover: [
+ *   {
+ *      elements: [document.getElementById("someelement")],
+ *      handler: ({ event, isInside }) => {
+ *        // do something here
+ *      }
+ *   }
+ * ]
+ *
+ * @param {Function} getClickHandlers - function that returns an array for click handlers (see above)
+ * @param {boolean} force - if `true`, the click handler will be called even if the click is inside (default: false)
+ */
+export function addClickHandlers(getClickHandlers, force = false) {
+  const handleClick = (event) => {
+    for (const element of getClickHandlers()) {
+      const isInside = (element.elements || []).reduce((total, current) => total || (current && current.contains(event.target)), false)
+      if (force || !isInside) {
+        element.handler({ event, isInside })
+      }
+    }
+  }
+  onMounted(() => {
+    document.addEventListener("click", handleClick)
+  })
+  onUnmounted(() => {
+    document.removeEventListener("click", handleClick)
+  })
 }
