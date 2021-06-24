50e32f574e365761f853bd302ca0a704adf545f7
Stefan Peters <stefandesu@exo.pm>
2021-06-24 14:45:59 +0200

    Small adjustments to Arrow

--- src/components/Arrow.vue
+++ src/components/Arrow.vue
@@ -1,6 +1,12 @@
 <template>
   <div
-    :class="`jskos-vue-arrow jskos-vue-arrow-${direction}`" />
+    :class="{
+      'jskos-vue-arrow': true,
+      'jskos-vue-arrow-clickable': clickable,
+    }">
+    <div
+      :class="`jskos-vue-arrow-${direction}`" />
+  </div>
 </template>
 
 <script>
@@ -16,6 +22,10 @@ export default defineComponent({
         return ["up", "down", "left", "right"].includes(value)
       },
     },
+    clickable: {
+      type: Boolean,
+      default: false,
+    },
   },
 })
 </script>
@@ -23,24 +33,34 @@ export default defineComponent({
 <style scoped>
 .jskos-vue-arrow {
   display: inline-block;
-  border: solid black;
+  vertical-align: middle;
+  padding: 5px 3px;
+}
+.jskos-vue-arrow > div {
+  border: solid var(--jskos-vue-arrow-color);
   border-width: 0 2px 2px 0;
   padding: 3px;
   margin: 2px 5px;
 }
-.jskos-vue-arrow.jskos-vue-arrow-right {
+.jskos-vue-arrow-clickable:hover {
+  cursor: pointer;
+}
+.jskos-vue-arrow-clickable:hover > div {
+  border-color: var(--jskos-vue-arrow-hover-color);
+}
+.jskos-vue-arrow > .jskos-vue-arrow-right {
   transform: rotate(-45deg);
   -webkit-transform: rotate(-45deg);
 }
-.jskos-vue-arrow.jskos-vue-arrow-left {
+.jskos-vue-arrow > .jskos-vue-arrow-left {
   transform: rotate(135deg);
   -webkit-transform: rotate(135deg);
 }
-.jskos-vue-arrow.jskos-vue-arrow-up {
+.jskos-vue-arrow > .jskos-vue-arrow-up {
   transform: rotate(-135deg);
   -webkit-transform: rotate(-135deg);
 }
-.jskos-vue-arrow.jskos-vue-arrow-down {
+.jskos-vue-arrow > .jskos-vue-arrow-down {
   transform: rotate(45deg);
   -webkit-transform: rotate(45deg);
 }
